364567981f1cf764039fc9d069497ae16b92fc53
leon58 <leon58.csie@gmail.com>
2020-03-02 00:58:16 +0800

    max_sum_array: upload brute force algo
    as title
    
    Signed-off-by: leon58 <leon58.csie@gmail.com>

--- max_sub_array.cpp
+++ max_sub_array.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 using namespace std;
 
-
+/* using kadane algo */
 class Solution {
 	public:
 		int maxSubArray(vector<int>& nums) {
@@ -21,10 +21,35 @@ class Solution {
 };
 
 
+class Solution_Brute {
+	public:
+		int maxSubArray(vector<int>& nums) {
+			int maxSum, i, j;
+
+			maxSum = nums[0];
+			
+			for (i = 0; i < nums.size(); i++)
+			{
+				int partial_sum = 0;
+
+				for (j = i; j < nums.size(); j++)
+				{
+					partial_sum += nums[j];
+					maxSum = max(partial_sum, maxSum);
+				}
+
+			}
+			return maxSum;
+		}
+};
+
+
+
 int main()
 {
 	vector<int> data;
 	Solution nice;
+	Solution_Brute bad;
 
 	data.push_back(-1);
 	data.push_back(5);
@@ -34,6 +59,7 @@ int main()
 	data.push_back(3);
 	data.push_back(2);
 
-	cout << "ans = " << nice.maxSubArray(data) << endl;
+	//cout << "ans = " << nice.maxSubArray(data) << endl;
+	cout << "ans = " << bad.maxSubArray(data) << endl;
 	return 0;
 }
