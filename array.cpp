32228f266fba786c89f23e5bbedfba4b7b605f25
leon58 <43083119+leon5842@users.noreply.github.com>
2021-12-16 06:46:19 +0800

    Update array.cpp

--- array.cpp
+++ array.cpp
@@ -118,6 +118,49 @@ vector<int> twoSum(vector<int>& nums, int target) {
 	return {};
 }
 
+/* remove zeros
+* Input: nums = [3,2,2,3], val = 3
+* Output: 2, nums = [2,2,_,_]
+* Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+* It does not matter what you leave beyond the returned k (hence they are underscores).
+*/
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i = 0;
+        int j = 0;
+        
+        while (i < nums.size()) {
+            if (nums[i])
+                swap(nums[i++], nums[j++]);
+            else
+                i++;
+        }
+    }
+};
+/* remove duplicate on sorted array
+* Input: nums = [0,0,1,1,1,2,2,3,3,4]
+* Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+* Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+* It does not matter what you leave beyond the returned k (hence they are underscores).
+*/
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i = 1;
+        int j = 0;
+        
+        while (i < nums.size()) {
+            if (nums[i] == nums[j])
+                i++;
+            else
+                swap(nums[i++], nums[++j]);
+                
+        }
+        return nums.empty() ? 0 : j + 1;
+    }
+};
+
 /*
  * two sum  (array was sorted)
  * assume the array was sorted in ascending order, find the two index (1 based instead of 0)
