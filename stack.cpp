1a46003e7108335688c9f377847ded8e842a22c8
leon58 <leon58.csie@gmail.com>
2021-12-28 01:21:29 +0800

    DS: update stack problem
    add problem
    
    Signed-off-by: leon58 <leon58.csie@gmail.com>

--- stack.cpp
+++ stack.cpp
@@ -154,3 +154,32 @@ bool validParenthesis(string s) {
 
 	return stk.empty() ? true : false;
 }
+
+
+/*
+ *
+ * Given a string s, return the lexicographically smallest subsequence of s that contains all the distinct characters of s exactly once.
+   Example 1:
+   Input: s = "bcabc"
+   Output: "abc"
+ *
+ */
+
+string smallestSubsequence(string s) {
+	string res;
+	int last[26] = {}, seen[26] = {}, n = s.size();
+	
+	for (int i = 0; i < n; i++)
+		last[s[i] - 'a'] = i;
+	
+	for (int i = 0; i < n; i+) {
+		if (seen[s[i] - 'a']++)
+			continue;
+		while (res.size() && res.back() > s[i] && i < last[res.back() - 'a']) {
+			seen[res.back() - 'a'] = 0;
+			res.pop_back();
+		}
+		res.push_back(s[i]);
+	}
+	return res;
+}
