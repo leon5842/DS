5f27843b9104beefb555bbe7689fed5aa7e524bb
leon58 <leon58.csie@gmail.com>
2021-12-29 22:30:50 +0800

    DS: update ds question
    as title
    
    Signed-off-by: leon58 <leon58.csie@gmail.com>

--- binary_search.cpp
+++ binary_search.cpp
@@ -86,3 +86,34 @@ int kthSmallest(vector<vector<int>> &matrix, int k) {
 	}
 	return left;
 }
+
+
+
+/*
+ *
+ * find peak element
+ * A peak element is an element that is strictly greater than its neighbors.
+ * Given an integer array nums, find a peak element, and return its index. 
+ * If the array contains multiple peaks, return the index to any of the peaks.
+ * You may imagine that nums[-1] = nums[n] = -∞.
+ *
+ * You must write an algorithm that runs in O(log n) time.
+ * Input: nums = [1,2,3,1]
+ * Output: 2
+ * Explanation: 3 is a peak element and your function should return the index number 2.
+ *
+ */
+
+int find PeakElement(vector<int> &nums) {
+	int l = 0;
+	int r = nums.size() - 1;
+
+	while (l < r) {
+		int mid = l + (r - l) / 2;
+		if (nums[mid] < nums[mid+1])
+			l = mid + 1;
+		else
+			r = mid;
+	}
+	return l;
+}
