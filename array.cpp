65502273451ed53b31bb34ae565c9ae44541b318
leon58 <leon58.csie@gmail.com>
2022-01-17 01:07:38 +0800

    DS: update for character replacement
    as title
    
    Signed-off-by: leon58 <leon58.csie@gmail.com>

--- array.cpp
+++ array.cpp
@@ -346,6 +346,18 @@ bool isUniqueChars(string s)
 }
 
 
+int characterReplacement(string s, int k) {
+	int maxf = 0, i = 0, n = s.length();
+	vector<int> count(26);
+	for (int j = 0; j < n; ++j) {
+		maxf = max(maxf, ++count[s[j] - 'A']);
+		if (j - i + 1 > maxf + k)
+			--count[s[i++] - 'A'];
+	}
+	return n - i;
+}
+
+
 int main() {
 
 	string ss = "abcdefgg";
