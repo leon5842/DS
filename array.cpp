6edc6f3bf8c7e897103ed1bcec6cdf5d7c42531d
leon58 <leon58.csie@gmail.com>
2022-01-21 00:52:49 +0800

    DS: update prefix sum problem
    as title
    
    Signed-off-by: leon58 <leon58.csie@gmail.com>

--- array.cpp
+++ array.cpp
@@ -386,6 +386,19 @@ int numOfSubstring(string s)
 }
 
 
+
+int subarraysDivByK(vector<int>& A, int K) {
+	vector<int> count(K);
+	count[0] = 1;
+	int prefix = 0, res = 0;
+	for (int a : A) {
+		prefix = (prefix + a % K + K) % K;
+		res += count[prefix]++;
+	}
+	return res;
+}
+
+
 int main() {
 
 	string ss = "abcdefgg";
